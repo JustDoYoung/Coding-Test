d167ee49c57a94286c1ad6c4d202021940457696
Young Kim <guckka94@gmail.com>
2024-03-12 23:10:41 +0900

    [level 1] Title: 달리기 경주, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -7,12 +7,10 @@ def solution(players, callings):
         winner_index = players_dict[calling]
         loser_index = players_dict[calling] - 1
 
-        loser_name = players[loser_index]
-        players[loser_index] = players[winner_index]
-        players[winner_index] = loser_name
-
-        players_dict[loser_name] += 1
+        players_dict[players[loser_index]] += 1
         players_dict[calling] -= 1
+
+        players[loser_index], players[winner_index] = players[winner_index], players[loser_index]
 
     answer = list(dict(sorted(players_dict.items(), key=lambda x: x[1])).keys())
     
