af29403ab261947ef10c523991b586bf9796b4b9
Young Kim <guckka94@gmail.com>
2024-03-14 22:28:37 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,8 +1,20 @@
-def solution(wall):
-    a, b = [], []
-    for i in range(len(wall)):
-        for j in range(len(wall[i])):
-            if wall[i][j] == "#":
-                a.append(i)
-                b.append(j)
-    return [min(a), min(b), max(a) + 1, max(b) + 1]
+def solution(wallpaper):
+    answer = []
+
+    drag_info = [0, 0, 0, 0]
+
+    file = [
+        (j, i)
+        for j, sub_list in enumerate(wallpaper)
+        for i, item in enumerate(sub_list)
+        if item == "#"
+    ]
+
+    j_axis = [j[0] for j in file]
+    i_axis = [i[1] for i in file]
+
+    min_axis = (min(j_axis), min(i_axis))
+    max_axis = (max(j_axis) + 1, max(i_axis) + 1)
+
+    answer = [*min_axis, *max_axis]
+    return answer
