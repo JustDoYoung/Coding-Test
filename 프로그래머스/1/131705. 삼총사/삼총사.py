52f66a0674425494b3cbd82089dbace344a313fe
Young Kim <guckka94@gmail.com>
2024-03-15 12:02:19 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,16 +1,7 @@
-def solution(number):
-    answer = 0
-    length = len(number)
-
-    case_container = tuple(
-        (number[x], number[y], number[z])
-        for x in range(0, length - 2)
-        for y in range(x + 1, length - 1)
-        for z in range(y + 1, length)
-    )
-
-    for each_case in case_container:
-        if sum(each_case) == 0:
-            answer += 1
-
-    return answer
+def solution (number) :
+    from itertools import combinations
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
+    return cnt
