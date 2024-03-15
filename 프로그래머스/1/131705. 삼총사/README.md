e3821236d01cd041f14dd879e75250e56eb81592
Young Kim <guckka94@gmail.com>
2024-03-15 12:05:33 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 10.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 12:02:45
+2024년 03월 15일 12:05:29
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,10 +1,15 @@
 def solution(number):
     answer = 0
-    l = len(number)
-    for i in range(l-2):
-        for j in range(i+1, l-1):
-            for k in range(j+1, l):
-                # print(number[i],number[j],number[k])
-                if number[i]+number[j]+number[k] == 0:
-                    answer += 1           
+    length = len(number)
+
+    case_container = tuple(
+        0
+        for x in range(0, length - 2)
+        for y in range(x + 1, length - 1)
+        for z in range(y + 1, length)
+        if number[x] + number[y] + number[z] == 0
+    )
+
+    answer = len(case_container)
+
     return answer
