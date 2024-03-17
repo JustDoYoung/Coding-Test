0f1a7d930a2c3874d8b5e42462eef9a0055290bc
Young Kim <guckka94@gmail.com>
2024-03-17 10:31:19 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,24 +1,18 @@
+from itertools import *
+
 def solution(babbling):
-    def SetCombination(count, can_speak_case, arr_one, arr_two):
-        length_one = len(arr_one)
-        length_two = len(arr_two)
-        for i in range(length_one):
-            can_speak_case.add(arr_one[i])
-            for j in range(length_two):
-                can_speak_case.add(arr_one[i] + arr_two[j])
-
-        count += 1
-        if count < length_one:
-            SetCombination(count, can_speak_case, arr_one, list(can_speak_case))
-
     answer = 0
-    word = ["aya", "ye", "woo", "ma"]
-    can_speak_case = set()
 
-    SetCombination(0, can_speak_case, word, word)
+    words = ["aya", "ye", "woo", "ma"]
+    words_length = len(words)
+    can_speak_set = set()
+
+    for i in range(1, words_length + 1):
+        for item in permutations(words, i):
+            can_speak_set.add("".join(item))
 
     for bab in babbling:
-        if bab in can_speak_case:
+        if bab in can_speak_set:
             answer += 1
 
     return answer
