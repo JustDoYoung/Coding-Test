035974d1496cde575ab33d06cd4aba9bc909cd89
Young Kim <guckka94@gmail.com>
2024-03-18 23:22:35 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,8 @@
+def solution(data, ext, val_ext, sort_by):
+
+    data_key = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
+    answer = [sub_list for sub_list in data if sub_list[data_key[ext]] <= val_ext]
+
+    answer.sort(key=lambda x: x[data_key[sort_by]])
+
+    return answer
