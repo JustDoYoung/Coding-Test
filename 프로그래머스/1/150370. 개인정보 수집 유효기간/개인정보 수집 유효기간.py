4b7355dcbf82aa9506223a798cc48140c188391a
Young Kim <guckka94@gmail.com>
2024-03-23 22:40:08 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,40 +1,12 @@
-def solution(today: str, terms: list, privacies: list):
-    answer = []
+def to_days(date):
+    year, month, day = map(int, date.split("."))
+    return year * 28 * 12 + month * 28 + day
 
-    today = today.split(".")
-    today_year = int(today[0])
-    today_month = int(today[1])
-    today_day = int(today[2])
-
-    terms_dict = {}
-
-    for term in terms:
-        term = term.split(" ")
-        terms_dict[term[0]] = int(term[1])
-
-    for i, case in enumerate(privacies):
-        case = case.split(" ")
-        splitted_case = case[0].split(".")
-
-        get_year = int(splitted_case[0])
-        get_month = int(splitted_case[1])
-        get_day = int(splitted_case[2])
-
-        # 몫은 year, 나머지는 month
-        offset = divmod(terms_dict[case[1]], 12)
-        print(offset)
-
-        limit_year = get_year + offset[0] + (get_month + offset[1] - 1) // 12
-        limit_month = (get_month + offset[1] - 1) % 12 + 1
-        print(limit_year, limit_month)
-
-        if (limit_year - today_year) < 0:
-            answer.append(i + 1)
-        elif (limit_year - today_year) == 0:
-            if (limit_month - today_month) < 0:
-                answer.append(i + 1)
-            elif (limit_month - today_month) == 0:
-                if (get_day - today_day) <= 0:
-                    answer.append(i + 1)
-
-    return answer
+def solution(today, terms, privacies):
+    months = {v[0]: int(v[2:]) * 28 for v in terms}
+    today = to_days(today)
+    expire = [
+        i + 1 for i, privacy in enumerate(privacies)
+        if to_days(privacy[:-2]) + months[privacy[-1]] <= today
+    ]
+    return expire
