d8dec9295c182af1a1eea4b4e1f1c7a1d1cffc4c
Young Kim <guckka94@gmail.com>
2024-03-24 15:04:24 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.59 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,10 +1,8 @@
-import math
-
 def solution(number, limit, power):
     answer = 0
 
     for knight in range(1, number + 1):
-        measure_range = int(math.sqrt(knight)) + 1
+        measure_range = int(knight**(0.5)) + 1
 
         weapon_power = 0
         for num in range(1, measure_range):
