6d4b315e92074168e5ab12a657ea7aca82dece1f
Young Kim <guckka94@gmail.com>
2024-03-25 23:49:40 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,17 +1,14 @@
+from collections import Counter
 def solution(X: str, Y: str):
     answer = ""
 
-    num_list = (str(num) for num in range(10))
+    num_list = [str(num) for num in range(9, -1, -1)]
 
-    common_nums = [
-        num
-        for num in num_list
-        for _ in range(min(X.count(num), Y.count(num)))
-        if num in X and num in Y
-    ]
+    x_dict = Counter(X)
+    y_dict = Counter(Y)
 
-    common_nums = sorted(common_nums, reverse=True)
-    answer = "".join(common_nums)
+    for num in num_list:
+        answer += num * min(x_dict[num], y_dict[num])
 
     if len(answer) == 0:
         answer = "-1"
