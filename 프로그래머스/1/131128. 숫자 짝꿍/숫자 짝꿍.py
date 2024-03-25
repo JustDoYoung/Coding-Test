5473d20fb51c22b5b564eb40e3c608a5271b92e0
Young Kim <guckka94@gmail.com>
2024-03-25 23:59:30 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,21 +1,29 @@
 def solution(X: str, Y: str):
     answer = ""
 
-    num_list = (str(num) for num in range(10))
+    X_dict = {}
+    Y_dict = {}
+    common_dict = {}
 
-    common_nums = [
-        num
-        for num in num_list
-        for _ in range(min(X.count(num), Y.count(num)))
-        if num in X and num in Y
-    ]
+    for x in X:
+        if X_dict.get(x) == None:
+            X_dict[x] = X.count(x)
 
-    common_nums = sorted(common_nums, reverse=True)
-    answer = "".join(common_nums)
+    for y in Y:
+        if Y_dict.get(y) == None:
+            Y_dict[y] = Y.count(y)
+
+    for key, x_count in X_dict.items():
+        if (y_count := Y_dict.get(key)) != None:
+            common_dict[key] = min(x_count, y_count)
+
+    answer = "".join(
+        [num * common_dict[num] for num in sorted(common_dict.keys(), reverse=True)]
+    )
 
     if len(answer) == 0:
         answer = "-1"
     elif answer[0] == "0":
-        answer = "0"
+        answer = str(int(answer))
 
     return answer
