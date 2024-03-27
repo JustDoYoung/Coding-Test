3213d427218b59fb0225a11df70118f4d8f62ae3
Young Kim <guckka94@gmail.com>
2024-03-27 21:38:18 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -2,7 +2,7 @@ def solution(n: int):
     n -= 1
     answer = n
 
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, n+1):
         if n % i == 0:
             answer = i
             break
