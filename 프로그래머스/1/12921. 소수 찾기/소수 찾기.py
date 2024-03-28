fce2d184465c4e3cce1507717401789000e5daf2
Young Kim <guckka94@gmail.com>
2024-03-28 20:55:32 +0900

    [level 1] Title: 소수 찾기, Time: 136.88 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -5,7 +5,7 @@ def solution(n):
 
     for i in range(2, int(n**0.5) + 1 + 1):
         if isPrime_list[i] == True:
-            for j in range(2, n // i + 1):
+            for j in range(i, n // i + 1):
                 isPrime_list[i * j] = False
 
     answer = isPrime_list.count(True) - 2
