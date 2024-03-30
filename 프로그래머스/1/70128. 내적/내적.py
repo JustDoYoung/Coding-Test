b434f7a4e9112a889f9e8e46fa928b0ae91c53dc
Young Kim <guckka94@gmail.com>
2024-03-30 23:40:08 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.py
+++ 프로그래머스/1/70128. 내적/내적.py
@@ -1,6 +1,8 @@
+import numpy
 def solution(a, b):
-    zip_list = zip(a, b)
-    map_list = list(map(lambda x: x[0] * x[1], zip_list))
-    answer = sum(map_list)
+    a = numpy.array(a)
+    b = numpy.array(b)
 
-    return answer
+    answer = numpy.sum(a * b)
+
+    return int(answer)
