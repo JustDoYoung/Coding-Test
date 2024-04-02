c5b0cbca1caca07ac60ef1adcadbae53a232973c
Young Kim <guckka94@gmail.com>
2024-04-02 22:31:43 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+
+    for com in commands:
+        temp = array[com[0] - 1 : com[1]]
+        temp = sorted(temp)
+        answer.append(temp[com[2] - 1])
+
+    return answer
