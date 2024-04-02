cb4e06e32068883c8ff2a1f23f55959e62326fb6
Young Kim <guckka94@gmail.com>
2024-04-02 22:21:53 +0900

    [level 1] Title: 모의고사, Time: 5.81 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,21 @@
 def solution(answers):
-    answer = []
     supo_1 = [1, 2, 3, 4, 5]
     supo_2 = [2, 1, 2, 3, 2, 4, 2, 5]
     supo_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    supo_score = {1: 0, 2: 0, 3: 0}
+    supo_score = [0, 0, 0]
 
     for i, ans in enumerate(answers):
         if supo_1[i % len(supo_1)] == ans:
-            supo_score[1] += 1
+            supo_score[0] += 1
         if supo_2[i % len(supo_2)] == ans:
-            supo_score[2] += 1
+            supo_score[1] += 1
         if supo_3[i % len(supo_3)] == ans:
-            supo_score[3] += 1
+            supo_score[2] += 1
 
-    supo_score = sorted(supo_score.items(), key=lambda x: -x[1])
-
-    for supo, score in supo_score:
-        if supo_score[0][1] == score:
-            answer.append(supo)
+    answer = []
+    for i, score in enumerate(supo_score):
+        if max(supo_score) == score:
+            answer.append(i + 1)
 
     return answer
