1bba73e73629887561fe131c68262ea8c033fe22
Young Kim <guckka94@gmail.com>
2024-04-04 17:17:04 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,20 +2,17 @@ def solution(n, arr1, arr2):
     answer = []
 
     for i in range(n):
+        bin_cross = str(bin(arr1[i] | arr2[i]))
 
-        bin_1 = str(bin(arr1[i]))
-        bin_2 = str(bin(arr2[i]))
+        bin_cross = bin_cross[2:].zfill(n)
 
-        bin_1 = bin_1[2:].rjust(n, "0")
-        bin_2 = bin_2[2:].rjust(n, "0")
-
-        map_temp = ""
-        for j in range(n):
-            if bin_1[j] == bin_2[j] and bin_1[j] == "0":
-                map_temp += " "
+        temp = ""
+        for b in bin_cross:
+            if b == "1":
+                temp += "#"
             else:
-                map_temp += "#"
+                temp += " "
 
-        answer.append(map_temp)
+        answer.append(temp)
 
     return answer
