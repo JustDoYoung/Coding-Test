9b8f46abb627606c5b2a507721de1446546eebf9
Young Kim <guckka94@gmail.com>
2024-04-04 22:43:51 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,20 +1,12 @@
 def solution(n, m):
     answer = []
-
     max_value = max(n, m)
     min_value = min(n, m)
 
-    for num in range(min_value, 0, -1):
-        if min_value%num == 0 and max_value % num == 0:
-            answer.append(num)
-            break
+    gr_cd = lambda x, y: y if x%y == 0 else gr_cd(y, x%y)
+    lst_cm = lambda x, y, cd: (x*y)//cd
 
-    i = 1
-    while True:
-        if (max_value*i) % min_value == 0:
-            answer.append(max_value*i)
-            break
-        i+=1
+    answer.append(gr_cd(max_value, min_value))
+    answer.append(lst_cm(max_value, min_value, answer[0]))
                 
-
     return answer
