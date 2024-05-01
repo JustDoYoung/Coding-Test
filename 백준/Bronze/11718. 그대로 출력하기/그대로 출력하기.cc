6c68510f39187c00b8af987fa205560798f0caa6
Young Kim <guckka94@gmail.com>
2024-05-01 22:48:52 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	char input[101];
+	while (cin.getline(input, 101)) {
+		cout << input << endl;
+	}
+
+	return 0;
+}
