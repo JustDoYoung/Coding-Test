6f3143479bfa5e3321b73117c8400346db5f3e13
Young Kim <guckka94@gmail.com>
2024-05-31 00:00:14 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    unsigned long long input;
+    cin >> input;
+
+    cout << input* input * input << '\n';
+    cout << 3 << '\n';
+
+    return 0;
+}
