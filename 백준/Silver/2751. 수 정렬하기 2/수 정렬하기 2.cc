920c363e80e154b393bd95e9f3edc1aac30e89bf
Young Kim <guckka94@gmail.com>
2024-06-11 20:15:10 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 288 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<int> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        int input;
+        cin >> input;
+
+        v.push_back(input);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i : v)
+        cout << i << '\n';
+
+    return 0;
+}
