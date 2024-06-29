4d6e41b133faf518501c446ccb880fe2e509fc02
Young Kim <guckka94@gmail.com>
2024-06-29 20:40:37 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    cout << (int)sqrt(n) << endl;
+
+    return 0;
+}
